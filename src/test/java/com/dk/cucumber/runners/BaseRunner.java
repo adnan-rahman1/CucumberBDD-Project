419d86f35040876582b0142da9826d76919c065d
Adnan Rahman <adnanibnamotin@gmail.com>
2021-01-30 22:29:53 -0500

    jenkin 008

--- src/test/java/com/dk/cucumber/runners/BaseRunner.java
+++ src/test/java/com/dk/cucumber/runners/BaseRunner.java
@@ -24,6 +24,6 @@ public class BaseRunner extends AbstractTestNGCucumberTests {
 
         @Test
         public void testSample() {
-                System.out.println("Running from jenkins 007");
+                System.out.println("Running from jenkins 008");
         }
 }
